d9a624a56f66b3c3d4d9b84e3a44948fd7923515
Stephen Howe <Stevehowe10@gmail.com>
2019-12-30 06:29:26 -0700

    front and back-end connected. Axios post and get working

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = 4000;
 let Todo = require('./todo.model');
 
 app.use(cors());
-app.use(bodyParser.json({ type: 'application/json'}));
+app.use(bodyParser.json());
 
 mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
